fa5a5de2b2a923241b007b109cc0e417b4bdd110
007AxelBerkati <bucel1916@gmail.com>
2022-10-14 08:01:00 +0700

    add icons, assets, and font

---
+++ portofolio-react-with-tailwind/src/components/Navbar.jsx
@@ -0,0 +1,40 @@
+import React from "react"
+import Logo from "../assets/logo.png"
+import { FaBars, FaTimes } from "react-icons/fa"
+
+const Navbar = () => {
+  return (
+    <div className="fixed w-full h-[80px] flex justify-between items-center px-4 bg-[#0a192f] text-gray-300">
+      <div>
+        <img src={Logo} alt="Logo Image" style={{ width: "50px" }} />
+      </div>
+
+      {/* {Menu} */}
+      <div>
+        <ul className="flex">
+          <li>Home</li>
+          <li>About</li>
+          <li>Skill</li>
+          <li>Work</li>
+          <li>Contact</li>
+        </ul>
+      </div>
+
+      {/* {Hmburger} */}
+      <div className="hidden">
+        <FaBars />
+      </div>
+
+      {/* {Mobile Menu} */}
+      <ul className="hidden">
+        <li>Home</li>
+        <li>About</li>
+        <li>Skill</li>
+        <li>Work</li>
+        <li>Contact</li>
+      </ul>
+    </div>
+  )
+}
+
+export default Navbar
