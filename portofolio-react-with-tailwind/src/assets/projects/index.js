cbff97f78930e3c3292131ec978c7e851d802570
007AxelBerkati <bucel1916@gmail.com>
2022-10-14 20:07:56 +0700

    Add image project

---
+++ portofolio-react-with-tailwind/src/assets/projects/index.js
@@ -0,0 +1,7 @@
+import MoviesStream from "./moviesStream.png"
+import MyBook from "./myBook.png"
+import MyChatting from "./myChatting.png"
+import MyPokemon from "./myPokemon.png"
+import SecondHand from "./secondHand.png"
+
+export { MoviesStream, MyBook, MyChatting, MyPokemon, SecondHand }
