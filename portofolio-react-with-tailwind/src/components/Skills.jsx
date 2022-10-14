86fdb2425cc56df794a2f5734511e96ae2d628fd
007AxelBerkati <bucel1916@gmail.com>
2022-10-14 20:07:21 +0700

    Skills Finish

---
+++ portofolio-react-with-tailwind/src/components/Skills.jsx
@@ -0,0 +1,77 @@
+import React from "react"
+import CSS from "../assets/css.png"
+import FireBase from "../assets/firebase.png"
+import GitHub from "../assets/github.png"
+import HTML from "../assets/html.png"
+import JavaScript from "../assets/javascript.png"
+import ReactImg from "../assets/react.png"
+import Tailwind from "../assets/tailwind.png"
+
+const Skills = () => {
+  return (
+    <div name="skills" className="w-full h-screen bg-[#0a192f] text-gray-300">
+      {/* Container */}
+      <div className="max-w-[1000px] mx-auto p-4 flex flex-col justify-center w-full h-full">
+        <div>
+          <p className="text-4xl font-bold inline border-b-4 border-pink-600 ">
+            Skills
+          </p>
+          <p className="py-4">These are the technologies I've worked with</p>
+        </div>
+
+        <div className="w-full grid grid-cols-2 sm:grid-cols-4 gap-4 text-center py-8">
+          <div className="shadow-md shadow-[#040c16] hover:scale-110 duration-500">
+            <a href="https://www.w3schools.com/html/s">
+              <img className="w-20 mx-auto" src={HTML} alt="HTML icon" />
+              <p className="my-4">HTML</p>
+            </a>
+          </div>
+          <div className="shadow-md shadow-[#040c16] hover:scale-110 duration-500">
+            <a href="https://www.w3schools.com/css/">
+              <img className="w-20 mx-auto" src={CSS} alt="HTML icon" />
+              <p className="my-4">CSS</p>
+            </a>
+          </div>
+          <div className="shadow-md shadow-[#040c16] hover:scale-110 duration-500">
+            <a href="https://www.w3schools.com/js/">
+              <img className="w-20 mx-auto" src={JavaScript} alt="HTML icon" />
+              <p className="my-4">JAVASCRIPT</p>
+            </a>
+          </div>
+          <div className="shadow-md shadow-[#040c16] hover:scale-110 duration-500">
+            <a href="https://reactjs.org/">
+              <img className="w-20 mx-auto" src={ReactImg} alt="HTML icon" />
+              <p className="my-4">REACT</p>
+            </a>
+          </div>
+          <div className="shadow-md shadow-[#040c16] hover:scale-110 duration-500">
+            <a href="https://reactnative.dev/">
+              <img className="w-20 mx-auto" src={ReactImg} alt="HTML icon" />
+              <p className="my-4">REACT NATIVE</p>
+            </a>
+          </div>
+          <div className="shadow-md shadow-[#040c16] hover:scale-110 duration-500">
+            <a href="https://github.com/007AxelBerkati">
+              <img className="w-20 mx-auto" src={GitHub} alt="HTML icon" />
+              <p className="my-4">GITHUB</p>
+            </a>
+          </div>
+          <div className="shadow-md shadow-[#040c16] hover:scale-110 duration-500">
+            <a href="https://tailwindcss.com/">
+              <img className="w-20 mx-auto" src={Tailwind} alt="HTML icon" />
+              <p className="my-4">TAILWIND</p>
+            </a>
+          </div>
+          <div className="shadow-md shadow-[#040c16] hover:scale-110 duration-500">
+            <a href="https://firebase.google.com/">
+              <img className="w-20 mx-auto" src={FireBase} alt="HTML icon" />
+              <p className="my-4">FIREBASE</p>
+            </a>
+          </div>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default Skills
