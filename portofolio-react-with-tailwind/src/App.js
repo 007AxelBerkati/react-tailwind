f029233e4952632b3939e84688a91a097dc99f2e
007AxelBerkati <bucel1916@gmail.com>
2022-10-14 20:08:30 +0700

    import all component to app js

--- portofolio-react-with-tailwind/src/App.js
+++ portofolio-react-with-tailwind/src/App.js
@@ -1,12 +1,20 @@
 import React from "react"
+import About from "./components/About"
+import Contact from "./components/Contact"
 import Home from "./components/Home"
 import Navbar from "./components/Navbar"
+import Skills from "./components/Skills"
+import Work from "./components/Work"
 
 const App = () => {
   return (
     <div className="text-xl font-bold">
       <Navbar />
       <Home />
+      <About />
+      <Skills />
+      <Work />
+      <Contact />
     </div>
   )
 }
