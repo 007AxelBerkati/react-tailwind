cfcbe2a5cf30d0b2aaa4fceb49834196051f38d6
007AxelBerkati <bucel1916@gmail.com>
2022-10-14 20:07:12 +0700

    Data Project

---
+++ portofolio-react-with-tailwind/src/data/data.js
@@ -0,0 +1,40 @@
+import {
+  MoviesStream,
+  MyBook,
+  MyChatting,
+  MyPokemon,
+  SecondHand,
+} from "../assets/projects"
+
+export const data = [
+  {
+    id: 1,
+    name: "Second Hand",
+    image: SecondHand,
+    github: "https://github.com/007AxelBerkati/final-project-RN-SecondHand",
+  },
+  {
+    id: 2,
+    name: "Movies Stream",
+    image: MoviesStream,
+    github: "https://gitlab.com/007AxelBerkati/movie_app",
+  },
+  {
+    id: 3,
+    name: "My Book",
+    image: MyBook,
+    github: "https://gitlab.com/007AxelBerkati/bookapp_redux_testing3",
+  },
+  {
+    id: 4,
+    name: "My Chatting",
+    image: MyChatting,
+    github: "https://gitlab.com/007AxelBerkati/collab-challlenge07",
+  },
+  {
+    id: 5,
+    name: "My Pokemon",
+    image: MyPokemon,
+    github: "https://github.com/007AxelBerkati/AppPokemon",
+  },
+]
